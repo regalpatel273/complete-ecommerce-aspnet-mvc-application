441530626c7d67997f2cb35eb59b314aeae1252d
Rigalkumar Sanjaybhai Patel <regalpatel273@users.noreply.github.com>
2023-06-30 12:21:20 -0400

    Added models,controllers and views

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Profile Picture")]
+        public string ProfilePictureUrl { get; set; }
+        [Display(Name = "Ful Name")]
+        public string FullName { get; set; }
+        [Display(Name = "Biography")]
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+    }
+}
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,15 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// DbContext Configuration
+builder.Services.AddDbContext<AppDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+}); 
 
 var app = builder.Build();
 
@@ -23,5 +31,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed Database
+AppDbInitializer.Seed(app);
 
 app.Run();
